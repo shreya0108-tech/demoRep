25e7abf2449d7b069b3db27ba3b72e9a458da503
shreya0108-tech <shreyaagarwal89.sa@gmail.com>
2025-05-21 19:57:27 +0530

    Changes in feature branch

--- src/main/java/com/proj/demo/DemoApplication.java
+++ src/main/java/com/proj/demo/DemoApplication.java
@@ -11,8 +11,13 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
 
         System.out.println(sum(2,3));
+        System.out.println(sum(2,3,4));
     }
     public static int sum(int a, int b) {
-        return a + b;
+        return a * b;
+    }
+
+    public static int sum(int a, int b, int c) {
+        return a + b + c;
     }
 }
